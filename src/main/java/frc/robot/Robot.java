8a64fb2626379a347d2a0fb9244d9de95563430c
ocartsonis <122648800+ocartsonis@users.noreply.github.com>
2024-01-20 18:39:09 -0700

    Finished Base Code
    Some things to note:
    -We definitely need to trial and error the motor numbers (or do it fr)
    -This project was set up as a command robot which seems to be a multi-script method of running the code (however I just deleted all of the command stuff so we don't have to worry about that unless we want to)
    -Hopefully the CANSparkMax class is compatible with the motorControl super class (although I am pretty certain it is)
    -WE TEST NEXT MEETING
    
    Co-Authored-By: NullTxt <90879949+nulltxt@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import com.revrobotics.CANSparkLowLevel;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
@@ -18,28 +16,26 @@ import com.revrobotics.CANSparkMax;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
-  
+
   private CANSparkMax motorFrontLeft = new CANSparkMax(1, CANSparkLowLevel.MotorType.kBrushed);
   private CANSparkMax motorFrontRight = new CANSparkMax(2, CANSparkLowLevel.MotorType.kBrushed);
   private CANSparkMax motorRearLeft = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushed);
   private CANSparkMax motorRearRight = new CANSparkMax(4, CANSparkLowLevel.MotorType.kBrushed);
 
-  DifferentialDrive frontDrive = new DifferentialDrive(motorFrontLeft, motorFrontRight);
-  DifferentialDrive rearDrive = new DifferentialDrive(motorRearLeft, motorRearRight);
+  private DifferentialDrive frontDrive = new DifferentialDrive(motorFrontLeft, motorFrontRight);
+  private DifferentialDrive rearDrive = new DifferentialDrive(motorRearLeft, motorRearRight);
 
-  PS5Controller controller = new PS5Controller(0);
+  private PS5Controller controller = new PS5Controller(0);
+
+  private boolean isDisabled = false;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+
   }
 
   /**
@@ -51,11 +47,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+    if(controller.getCrossButton())
+    {
+      isDisabled = true;
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -68,12 +63,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
   /** This function is called periodically during autonomous. */
@@ -82,23 +72,29 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if(isDisabled)
+    {
+      motorFrontLeft.stopMotor();
+      motorFrontRight.stopMotor();
+      motorRearLeft.stopMotor();
+      motorRearRight.stopMotor();
+    }
+    else
+    {
+      frontDrive.arcadeDrive(controller.getRightX(), controller.getRightY());
+      frontDrive.arcadeDrive(controller.getRightX(), controller.getRightY());
+    }
+  }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
+
   }
 
   /** This function is called periodically during test mode. */
